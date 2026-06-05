c077bcb7cf9663269f0090e0ad7cec8d39b263a3
rpbrasil <rntprch@gmail.com>
2026-06-05 15:06:21 -0300

    corrigido erro de build

--- app/components/dashboard/DashboardShell.tsx
+++ app/components/dashboard/DashboardShell.tsx
@@ -17,7 +17,10 @@ export function DashboardShell({ children }: { children: React.ReactNode }) {
         {/* Conteúdo */}
         <div className="flex-1 flex flex-col">
           {/* Header */}
-          <DashboardHeader onMenuClick={() => setMenuOpen(true)} />
+          <DashboardHeader
+            onMenuClick={() => setMenuOpen(true)}
+            isMenuOpen={menuOpen}
+          />
 
           {/* Main */}
           <main className="flex-1 p-6 overflow-y-auto">
