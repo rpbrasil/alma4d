fbb651f8eb18cb45fe14b23fabf181f564a5cf94
rpbrasil <rntprch@gmail.com>
2026-06-07 08:35:25 -0300

    open graph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,34 @@ import PwaIosBanner from "@/components/PwaIosBanner";
 
 export const metadata = {
   metadataBase: new URL("https://alma4d.com"),
-  title: "alma4D Express",
-  description: "Gestão de riscos, documentos e questionários",
 
-  // ✅ PWA
+  title: "alma4D Express",
+  description: "Gestão de riscos, NR1, riscos psicossociais",
+
+  openGraph: {
+    title: "alma4D Express",
+    description: "Gestão de riscos, NR1, riscos psicossociais",
+    url: "https://alma4d.com",
+    siteName: "alma4D",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "alma4D Express",
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "alma4D Express",
+    description: "Gestão de riscos, documentos e questionários",
+    images: ["/og-image.png"],
+  },
+
   manifest: "/app.webmanifest",
 
   icons: [
@@ -19,8 +43,6 @@ export const metadata = {
       sizes: "192x192",
     },
     { rel: "apple-touch-icon", url: "/icons/icon-192.png", sizes: "192x192" },
-    { rel: "apple-touch-startup-image", url: "/icons/icon-512.png" },
-    { rel: "mask-icon", url: "/icons/icon-512-maskable.png", color: "#030870" },
   ],
 
   appleWebApp: {
